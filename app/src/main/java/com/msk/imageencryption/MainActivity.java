72bd669aa31d041c14d70c65ab898b3d64aa83ee
M1saki <897540755@qq.com>
2019-04-05 15:59:08 +0800

    init project

---
+++ app/src/main/java/com/msk/imageencryption/MainActivity.java
@@ -0,0 +1,13 @@
+package com.msk.imageencryption;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
